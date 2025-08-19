72bda7194e7cf22dac7ce86e08495d2e04ea21de
unknown <devmnut@gmail.com>
2025-08-19 15:29:38 +0700

    first commit

---
+++ api/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ MongoDB Connected"))
+  .catch(err => console.error(err));
+
+// Routes
+const productRoutes = require("../routes/productRoutes");
+app.use("/api/products", productRoutes);
+
+module.exports = app; // do not use app.listen()
